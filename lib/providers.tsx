aa7d746e5fa0d26640af2df217bf0f33f058735b
Le Viet-Hoang <hashdotlee@gmail.com>
2023-12-22 10:55:01 +0700

    fix profile

--- lib/providers.tsx
+++ lib/providers.tsx
@@ -1,19 +1,12 @@
 "use client";
 
 import { SessionProvider } from "next-auth/react";
-import { Session } from "next-auth/types";
 import { DndProvider } from "react-dnd";
 import { HTML5Backend } from "react-dnd-html5-backend";
 
-export default function Providers({
-  children,
-  session,
-}: {
-  children: React.ReactNode;
-  session: Session | null;
-}) {
+export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    <SessionProvider session={session}>
+    <SessionProvider>
       <DndProvider backend={HTML5Backend}>{children}</DndProvider>
     </SessionProvider>
   );
